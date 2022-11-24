b18c46835013d0c3003ab3ae4bccc216f68961eb
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-24 13:16:09 +0530

    included nameInput component

---
+++ client/src/components/NameInput.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<main>
+    <div>
+        <label for="name">Enter name</label>
+        <input type="text">
+    </div>
+    <div>
+        <button>Set Name</button>
+    </div>
+</main>
+
+<style>
+    
+</style>
