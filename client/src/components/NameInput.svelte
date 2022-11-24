b6fbbdc6eedd60d7cc1d119328777671d33bd62a
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-24 19:05:03 +0530

    completed nameInput component static parts

--- client/src/components/NameInput.svelte
+++ client/src/components/NameInput.svelte
@@ -3,20 +3,33 @@
 </script>
 
 <main>
-    <div>
+    <div class="inpDiv">
         <input type="text" placeholder="Enter name">
-        <button>Set Name</button>
+        <button id="setName">Set Name</button>
     </div>
+    <h1>Hello User</h1>
+    <div class="startDiv">
+        <button class="start">Get Started with the Chat</button>
+    </div>
+    <p>Set a name first!</p>
 </main>
 
 <style>
-    div{
+    main{
+        padding: 10px;
+    }
+    div.inpDiv{
         display: flex;
         justify-content: center;
         align-items: center;
         margin: 20px auto;
         gap: 30px;
-        padding: 10px;
+    }
+
+    div.startDiv{
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 
     input{
@@ -32,15 +45,29 @@
     }
 
     button{
-        height: 50px;
-        border-radius: 5px;
-        border-width: 0;
-        flex: 1;
         background-color: #9aff7e;
         font-family: 'Lato',sans-serif;
         font-weight: 800;
         letter-spacing: 1px;
         font-size: 16px;
         cursor: pointer;
+        height: 50px;
+        border-radius: 5px;
+        border-width: 0;
     }
+
+    button#setName{
+        flex: 1;
+        background-color: #7ec5ff;
+    }
+
+    button.start{
+        padding: 10px 40px;
+    }
+
+    p{
+        color: red;
+        text-align: center;
+    }
+
 </style>
