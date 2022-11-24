709ccfcfbf38f6aa66376cbdbbfe079857e7c344
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-24 12:37:43 +0530

    header component

---
+++ client/src/components/Header.svelte
@@ -0,0 +1,18 @@
+<script>
+    import chatStreetLogo from './../assets/chat_street.png'
+</script>
+
+<main>
+    <img src={chatStreetLogo} alt="logo">
+</main>
+
+<style>
+    main{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    img{
+        width: 30%;
+    }
+</style>
