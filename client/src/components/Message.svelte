fff8411b97e565feabec2ca9c31f8a041aa424bd
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-27 23:24:24 +0530

    basic layout of message component

---
+++ client/src/components/Message.svelte
@@ -0,0 +1,28 @@
+<script>
+
+</script>
+
+<main>
+    <p class="msg">
+        <span class="person">Jackob</span><br>
+        <span>How are you?</span>
+    </p>
+</main>
+
+<style>
+    /* main{
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+    } */
+    p.msg{
+        padding: 10px;
+        border-radius: 8px;
+        background-color: aquamarine;
+        margin: 10px;
+        width: fit-content;
+    }
+    span.person{
+        font-size: 12px;
+    }
+</style>
