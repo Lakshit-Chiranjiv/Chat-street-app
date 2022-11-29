4d2611a4813f61bc34dcd019578dba4d37c37c6b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-29 16:58:31 +0530

    flex on main and self-msg class to align flex end

--- client/src/components/Message.svelte
+++ client/src/components/Message.svelte
@@ -10,17 +10,20 @@
 </main>
 
 <style>
-    /* main{
+    main{
         display: flex;
         flex-direction: column;
-        align-items: flex-end;
-    } */
+    }
     p.msg{
         padding: 10px;
         border-radius: 8px;
         background-color: aquamarine;
         margin: 10px;
         width: fit-content;
+    }
+
+    p.self-msg{
+        align-self: flex-end;
     }
     span.person{
         font-size: 12px;
