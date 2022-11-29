358c7187bd7fbed7e1c65af29e61d7feb3c981b6
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-29 17:36:38 +0530

    userName variable

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,14 +1,14 @@
 <script>
   import Chat from "./components/Chat.svelte";
-import Header from "./components/Header.svelte";
+  import Header from "./components/Header.svelte";
   import NameInput from "./components/NameInput.svelte";
-
+  let userName = ''
 
 </script>
 
 <main>
   <Header/>
-  <h1>Hello User</h1>
+  <h1>Hello {userName ? userName : 'User'}</h1>
   <NameInput/>
   <Chat/>
 </main>
