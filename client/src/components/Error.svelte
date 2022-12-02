583eec3898ab18ecf0631d970d6c8ce7a9383f9b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-12-02 12:29:02 +0530

    error component created

---
+++ client/src/components/Error.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let errMsg = 'Some error'
+</script>
+
+<main>
+    <p>Error occured : {errMsg}</p>
+</main>
+
+<style>
+    main{
+        background-color: #ff9061;
+        border-radius: 10px;
+        border: 2px solid red;
+        padding: 16px;
+    }
+    
+    p{
+        color: white;
+    }
+</style>
