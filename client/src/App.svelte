3d49037e589c04b885dbf051bfd0dddaf0e60938
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-12-02 11:51:46 +0530

    connected to graphql client

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,11 +2,20 @@
   import Chat from "./components/Chat.svelte";
   import Header from "./components/Header.svelte";
   import NameInput from "./components/NameInput.svelte";
+  
+  import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+  import { setClient } from 'svelte-apollo'
 
   import { Router, Route } from "svelte-routing";
 
   let userName = ''
   let showError = false
+
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache(),
+});
+  setClient(client);
 
   const setUserName = (e) => {
     userName = e.detail.name
