41fd07a5d84bb177c9f298504a01a0cfb243172e
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-12-04 23:42:23 +0530

    handler configured for empty inputs

--- client/src/components/Chat.svelte
+++ client/src/components/Chat.svelte
@@ -22,16 +22,18 @@
     const sendMessage = mutation(SEND_MSG);
 
     const handleMessageSend = async() => {
-        try {
-            await sendMessage({ variables:  {
-                input: {
-                    user: userName,
-                    msg: msgText,
-                    type: 3
-                }
-            }});
-        } catch (error) {
-            console.log("Some error occured")
+        if(userName && msgText){
+            try {
+                await sendMessage({ variables:  {
+                    input: {
+                        user: userName,
+                        msg: msgText,
+                        type: 3
+                    }
+                }});
+            } catch (error) {
+                console.log("Some error occured")
+            }
         }
     }
 </script>
