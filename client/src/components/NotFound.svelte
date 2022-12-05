d7b75a8807f63863bd5008a5dd8318f78ae4b28f
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-12-05 22:56:31 +0530

    NotFound component

---
+++ client/src/components/NotFound.svelte
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<main>
+    <h2>404 Page Not Found</h2>
+    <button>Go to Home</button>
+</main>
+
+<style>
+    main{
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
+        justify-content: center;
+        align-items: center;
+    }
+
+    button{
+        padding: 12px 24px;
+        border-radius: 10px;
+        background-color: rgb(146, 204, 255);
+        border-width: 0;
+        font-family: 'Lato',sans-serif;
+        cursor: pointer;
+        font-weight: 700;
+        letter-spacing: 1px;
+    }
+</style>
